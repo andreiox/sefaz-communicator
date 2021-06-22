c1574523d07f30447b3d9540daad3754da982ac2
Cristiano <neivacristiano@yahoo.com.br>
2021-06-22 13:39:29 -0300

    fix: interface customFormatLocation

--- index.d.ts
+++ index.d.ts
@@ -9,7 +9,7 @@ interface SefazCommunicatorOptions {
   forceSoap12Headers?: boolean;
   contentType?: string;
   proxy?: string;
-  formatLocation?: (location: string, isHttps: boolean) => string;
+  customFormatLocation?: (location: string, isHttps: boolean) => string;
 }
 
 export function communicate(
