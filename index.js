af63d3e2707feaafaf800ef2642763da4dc3314c
lucaspere <lucasrupestre@hotmail.com>
2021-10-01 10:48:15 -0300

    fix: handle wsdl with multiple ports and binding

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const createSoapClient = async (url, options, isHttps) => {
 
 const createSoapMethod = (client, methodName, isHttps, customFormatLocation) => {
   const service = Object.values(client.wsdl.definitions.services)[0];
-  const port = Object.values(service.ports)[0];
+  const port = Object.values(service.ports).find(port => port.binding.methods[methodName]);
 
   const method = port.binding.methods[methodName];
   const location = formatLocation(port.location, isHttps, customFormatLocation);
